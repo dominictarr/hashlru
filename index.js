4dbe6fefa681a6b52a0747481d2fef123bcb5881
Dominic Tarr <dominic.tarr@gmail.com>
2016-12-24 00:03:03 +0000

    nicer code

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-
-
 module.exports = function (max) {
-
   var size = 0, cache = {}, _cache = {}
+
+  function update (key, value) {
+    cache[key] = value
+    size ++
+    if(size >= max) {
+      size = 0
+      _cache = cache
+      cache = {}
+    }
+  }
 
   return {
     get: function (key) {
       var v = cache[key]
       if(v) return v
-      if(v = _cache[key]) {
-        cache[key] = v
-        size ++
-        if(size >= max) {
-          size = 0
-          _cache = cache
-          cache = {}
-        }
-      }
+      if(v = _cache[key]) update(key, v)
     },
     set: function (key, value) {
-      if(cache[key]) {
-        cache[key] = value
-      } else {
-        cache[key] = value
-        size ++
-        if(size >= max) {
-          size = 0
-          _cache = cache
-          cache = {}
-        }
-      }
+      if(cache[key]) cache[key] = value
+      else update(key, value)
     }
   }
 }
+
+
+
+
+
+
+
 
 
 
