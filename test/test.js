b27af392cda3e93254e08f7c8f71ae0bcdc9cd6e
davidmarkclements <huperekchuno@googlemail.com>
2016-12-31 18:37:29 +0000

    tests

---
+++ test/test.js
@@ -0,0 +1,30 @@
+var assert = require('assert')
+var HLRU = require('../')
+var lru = HLRU(2)
+
+// set-get:
+lru.set('test', 'test')
+
+assert.equal(lru.get('test'), 'test')
+
+// update:
+lru.set('test', 'test2')
+
+assert.equal(lru.get('test'), 'test2')
+
+// cache cycle:
+lru.set('test2', 'test')
+
+assert.equal(lru.get('test2'), 'test')
+
+// get previous after cache cycle:
+assert.equal(lru.get('test'), 'test2')
+
+// update new cache:
+lru.set('test2', 'test2')
+
+assert.equal(lru.get('test2'), 'test2')
+
+// object purity:
+
+assert.equal(lru.get('constructor'), undefined)
